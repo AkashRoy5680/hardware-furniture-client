5f41aafc416e447c93a47667fa8d474e4b47ae8b
AkashRoy5680 <royakash1272@gmail.com>
2022-05-25 04:55:02 +0200

    business and footer added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import Business from './Business';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Business></Business>
         </div>
     );
 };
