ced960eef9c2c01d177e2bb48679c1497ca6c386
AkashRoy5680 <royakash1272@gmail.com>
2022-05-28 08:44:45 +0200

    website is responsive for mobile version

--- src/Pages/Home/Business.js
+++ src/Pages/Home/Business.js
@@ -5,22 +5,32 @@ const Business = () => {
       
     <div>
       <h2 className='text-2xl text-primary font-bold m-5'>Business Summary</h2>
-     <div class="stats shadow">
-  <div class="stat place-items-center">
+  <div class="stats shadow grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  ">
+  
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    </div>
     <div class="stat-title">Downloads</div>
-    <div class="stat-value text-primary">31K</div>
-    <div class="stat-desc">From January 1st to February 1st</div>
+    <div class="stat-value">31K</div>
+    <div class="stat-desc">Jan 1st - Feb 1st</div>
   </div>
   
-  <div class="stat place-items-center">
-    <div class="stat-title">Users</div>
-    <div class="stat-value text-secondary">4,200</div>
-    <div class="stat-desc text-secondary">↗︎ 40 (2%)</div>
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+    </div>
+    <div class="stat-title">New Users</div>
+    <div class="stat-value">4,200</div>
+    <div class="stat-desc">↗︎ 400 (22%)</div>
   </div>
   
-  <div class="stat place-items-center">
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+    </div>
     <div class="stat-title">New Registers</div>
-    <div class="stat-value text-accent">1,200</div>
+    <div class="stat-value">1,200</div>
     <div class="stat-desc">↘︎ 90 (14%)</div>
   </div>
   
--- src/Pages/MyPortfoilo/MyPortfolio.js
+++ src/Pages/MyPortfoilo/MyPortfolio.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React from "react";
+import DP from "../MyPortfoilo/DP.JPEG";
 
 const MyPortfolio = () => {
-    return (
-        <div>
-            <h2 className='text-2xl text-accent font-bold mb-3'>My Portfolio</h2>
+  return (
+    <div>
+      <h2 className="text-2xl text-accent font-bold mb-3">My Portfolio</h2>
+      {/* <p> <span className='text-secondary font-bold'> Name</span>: Akash Saha</p>
+            <p><span className='text-secondary font-bold'>Email</span>: royakash1272@gmail.com</p>
+            <p><span className='text-secondary font-bold'>Educational Background</span>: Masters in Information Technology</p>
+            <p><span className='text-secondary font-bold'>Skills</span>: HTML5,CSS3,REACT JS,Tailwind CSS,Firebase,MongoDB,Node JS,
+            Github
+            </p>
+            <p><span className='text-secondary font-bold'>Website Links: </span>
+           <br/> <a href="https://furniture-hub-client.netlify.app/">https://furniture-hub-client.netlify.app/</a>
+           <br/> <a href="https://independent-service-supplier.netlify.app/">https://independent-service-supplier.netlify.app/</a>
+           <br/> <a href="https://gadget-router-assignment-9.netlify.app/">https://gadget-router-assignment-9.netlify.app/</a>
+    </p>*/}
+
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row">
+          <img style={{height:"250px"}} src={DP} alt=""
+            class="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <p class="py-6">
             <p> <span className='text-secondary font-bold'> Name</span>: Akash Saha</p>
             <p><span className='text-secondary font-bold'>Email</span>: royakash1272@gmail.com</p>
             <p><span className='text-secondary font-bold'>Educational Background</span>: Masters in Information Technology</p>
             <p><span className='text-secondary font-bold'>Skills</span>: HTML5,CSS3,REACT JS,Tailwind CSS,Firebase,MongoDB,Node JS,
             Github
             </p>
-            <p><span className='text-secondary font-bold'>Website Links:</span></p>
-
+            <p><span className='text-secondary font-bold'>Website Links: </span>
+           <br/> <a href="https://furniture-hub-client.netlify.app/">https://furniture-hub-client.netlify.app/</a>
+           <br/> <a href="https://independent-service-supplier.netlify.app/">https://independent-service-supplier.netlify.app/</a>
+           <br/> <a href="https://gadget-router-assignment-9.netlify.app/">https://gadget-router-assignment-9.netlify.app/</a>
+    </p>
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MyPortfolio;
